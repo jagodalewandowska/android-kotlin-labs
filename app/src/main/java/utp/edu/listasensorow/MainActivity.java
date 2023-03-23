c63fcb5263c2f7734dc644254a8e4e874aaea2f3
jagodalewandowska <jadlewandowska@gmail.com>
2023-03-23 15:20:54 +0100

    lab3

--- app/src/main/java/utp/edu/listasensorow/MainActivity.java
+++ app/src/main/java/utp/edu/listasensorow/MainActivity.java
@@ -18,7 +18,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     float[] orientation = new float[3];
     private SensorManager mSensorManager;
     private Sensor mRotationV;
-
+    // Magnetic field sensor
+    private Sensor msensor;
+    private float[] mGeomagnetic = new float[3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +29,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         mRotationV = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+        msensor = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
         boolean listener = mSensorManager.registerListener(this, mRotationV, SensorManager.SENSOR_DELAY_UI);
+        boolean mlistener = mSensorManager.registerListener(this, msensor, SensorManager.SENSOR_DELAY_GAME);
 
         List<Sensor> sensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
         TextView sensorList = findViewById(R.id.sensorList);
@@ -39,14 +43,33 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
 
     @Override
-    public void onSensorChanged(SensorEvent event){
-        SensorManager.getRotationMatrixFromVector(rMat, event.values);
-        mAzimuth = (int) (Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[0]) + 360) % 360;
+    public void onSensorChanged(SensorEvent event) {
 
-        mAzimuth = Math.round(mAzimuth);
+        synchronized (this) {
 
-        TextView azimuthValue = findViewById(R.id.azimuth);
-        azimuthValue.setText(String.valueOf(mAzimuth));
+            if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
+                // azymut
+                SensorManager.getRotationMatrixFromVector(rMat, event.values);
+                mAzimuth = (int) (Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[0]) + 360) % 360;
+                mAzimuth = Math.round(mAzimuth);
+
+                TextView azimuthValue = findViewById(R.id.azimuth);
+                azimuthValue.setText(String.valueOf(mAzimuth));
+            }
+
+            if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+                // magnetic sensor
+                mGeomagnetic = event.values;
+
+                TextView mX = findViewById(R.id.mx);
+                TextView mY = findViewById(R.id.my);
+                TextView mZ = findViewById(R.id.mz);
+
+                mX.setText(String.valueOf(mGeomagnetic[0]));
+                mY.setText(String.valueOf(mGeomagnetic[1]));
+                mZ.setText(String.valueOf(mGeomagnetic[2]));
+            }
+        }
     }
 
 
