8a1c5881440e519241deb2cb1935a9c3bdf0c2e5
jagodalewandowska <jadlewandowska@gmail.com>
2023-04-17 09:34:24 +0200

    Lab6 init

---
+++ Lab6/app/src/main/java/utp/edu/lab6/ui/theme/MainActivity.kt
@@ -0,0 +1,12 @@
+package utp.edu.lab6.ui.theme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import utp.edu.lab6.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
