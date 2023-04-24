8740bd48d3e04bba982b800cf56357e0308edd3c
jagodalewandowska <jadlewandowska@gmail.com>
2023-04-24 09:51:36 +0200

    no errors wild

--- Lab7/app/src/main/java/utp/edu/lab7/screens/home/HomeScreens.kt
+++ Lab7/app/src/main/java/utp/edu/lab7/screens/home/HomeScreens.kt
@@ -3,6 +3,7 @@ package utp.edu.lab7.screens.home
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
